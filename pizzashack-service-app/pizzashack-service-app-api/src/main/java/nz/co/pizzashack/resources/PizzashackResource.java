43afec98e222e279dae23a31067a282b1e53bb43
davidy <david.yuan124@gmail.com>
2015-01-27 18:25:57 +1300

    tst

--- pizzashack-service-app/pizzashack-service-app-api/src/main/java/nz/co/pizzashack/resources/PizzashackResource.java
+++ pizzashack-service-app/pizzashack-service-app-api/src/main/java/nz/co/pizzashack/resources/PizzashackResource.java
@@ -3,11 +3,18 @@ package nz.co.pizzashack.resources;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import nz.co.pizzashack.model.Pizzashack;
 import nz.co.pizzashack.repository.PizzashackRepository;
--- pizzashack-service-app/pizzashack-service-app-shared/src/main/java/nz/co/pizzashack/config/ConfigurationServiceModule.java
+++ pizzashack-service-app/pizzashack-service-app-shared/src/main/java/nz/co/pizzashack/config/ConfigurationServiceModule.java
@@ -23,7 +23,7 @@ public class ConfigurationServiceModule extends AbstractModule {
 			properties.load(new FileReader(Resources.getResource("pizzashack.properties").getFile()));
 			Names.bindProperties(binder(), properties);
 		} catch (final FileNotFoundException e) {
-			LOGGER.error("The configuration file image.properties can not be found", e);
+			LOGGER.error("The configuration file pizzashack.properties can not be found", e);
 		} catch (final IOException e) {
 			LOGGER.error("I/O Exception during loading configuration", e);
 		}
