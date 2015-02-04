314f5b18b300ba3b7feba0062ef1a6084eaf7dbe
davidy <david.yuan@propellerhead.co.nz>
2015-02-04 14:30:37 +1300

    test

--- pizzashack-web/app/scripts/pizza-admin-ui.js
+++ pizzashack-web/app/scripts/pizza-admin-ui.js
@@ -3,9 +3,11 @@
 angular.module('pizzaAdmin')
 .directive('pizzaList',function(){
         return {
+            restrict: 'E',
+            transclude: true,
             scope: {
-                pitem: '=pitem'
             },
-            template: '<th style="font-weight:normal;">{{pitem.pizzashackId}}</th><th style="font-weight:normal;">{{pitem.pizzaName}}</th><th style="font-weight:normal;">$ {{pitem.price}}</th><th><a href="#/{{pitem.pizzashackId}}" class="btn btn-info" role="button">Info</a> <button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-warning">Delete</button></th>'
+            template: '<th style="font-weight:normal;">{{val.pizzashackId}}</th><th style="font-weight:normal;">{{val.pizzaName}}</th><th style="font-weight:normal;">$ {{val.price}}</th><th><a href="#/{{val.pizzashackId}}" class="btn btn-info" role="button">Info</a> <button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-warning">Delete</button></th>'
+
         };
 });
